4cf4dfdea56253dd636f73136942ef2b43e7ab25
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-01-05 13:43:35 -0700

    adding audio to the first element

--- drum_kit_day1/script.js
+++ drum_kit_day1/script.js
@@ -1,5 +1,12 @@
 console.log("hello");
 
 window.addEventListener("keydown", function(e) {
-    console.log(e);
+    const audio = document.querySelectorAll(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelectorAll(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) {
+        return;
+    }
+    audio[0].currentTime = 0
+    audio[0].play();
+    key[0].classList.add('playing');
 });
