e06bd2819fd6220bc0f506c366c00d9deaa188bd
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-01-05 14:02:49 -0700

    more of the styles are in place

--- drum_kit_day1/script.js
+++ drum_kit_day1/script.js
@@ -1,12 +1,19 @@
-console.log("hello");
-
 window.addEventListener("keydown", function(e) {
     const audio = document.querySelectorAll(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelectorAll(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) {
-        return;
-    }
+    if (!audio) return;
     audio[0].currentTime = 0
     audio[0].play();
     key[0].classList.add('playing');
+});
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll(".key");
+console.log(keys);
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
 });
