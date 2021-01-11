cf299b2462df03dea9d3925054d64999330bf9dc
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-01-11 12:22:34 -0700

    slide in on scroll finished

--- slide_scroll_day13/script.js
+++ slide_scroll_day13/script.js
@@ -1,14 +1,34 @@
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-  }
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach(slider => {
+    // half way through the image.
+    const slideInAt = (window.scrollY + window.innerHeight) - slider.height / 2;
+    // bottom through the image.
+    const imageBottom = slider.offsetTop + slider.height;
+    const isHalfShown = slideInAt > slider.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      slider.classList.add('active');
+    } else {
+      slider.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
