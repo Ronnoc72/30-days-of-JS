8c20699b18a7ee1b2cc244a0fadaa45ec5f16251
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-01-10 18:15:44 -0700

    dev tools project complete

--- dev_tools_day9/script.js
+++ dev_tools_day9/script.js
@@ -7,25 +7,39 @@ function makeGreen() {
 }
 
 // Regular
-
+console.log('Hello');
 // Interpolated
-
+console.log('Hello I am a', 'cool guy');
 // Styled
-
+console.log("%c I am some great text.", "font-size: 10px; background: red;");
 // warning!
-
+console.warn("Oh No");
 // Error :|
-
+console.error("Crap");
 // Info
-
+console.info("Cake is my favorite dessert");
 // Testing
-
+console.assert(1 === 2, "It is wrong");
 // clearing
-
+console.clear();
 // Viewing DOM Elements
-
+p = document.querySelector('p');
+console.dir(p);
 // Grouping together
-
+dogs.forEach(dog => {
+  console.group(`${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old.`);
+  console.groupEnd(`${dog.name}`);
+})
 // counting
-
+console.count('bill');
+console.count('connor');
+console.count('bill');
+console.count('connor');
+console.count('connor');
 // timing
+console.time("fetching data");
+// fetch the data.
+console.timeEnd("fetching data");
+// table
+console.table(dogs);
