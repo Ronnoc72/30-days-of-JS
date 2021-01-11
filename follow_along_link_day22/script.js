fca34bf91ee8a109a32f8d12f6a07bfdb9e9576c
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-01-11 14:14:51 -0700

    still need to finish

---
+++ follow_along_link_day22/script.js
@@ -0,0 +1,14 @@
+// 👀
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    highlight.style.width
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
