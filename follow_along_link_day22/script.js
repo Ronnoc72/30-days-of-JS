2a513e81f55826b1cde3f2d0ef6ba9a00cbd3151
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2021-01-13 12:43:21 -0700

    adding days 22 and 23

--- follow_along_link_day22/script.js
+++ follow_along_link_day22/script.js
@@ -7,8 +7,15 @@ document.body.append(highlight);
 
 function highlightLink() {
     const linkCoords = this.getBoundingClientRect();
-    console.log(linkCoords);
-    highlight.style.width
+    const cords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    }
+    highlight.style.width = `${cords.width}px`;
+    highlight.style.height = `${cords.height}px`;
+    highlight.style.transform = `translate(${cords.left}px, ${cords.top}px)`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
